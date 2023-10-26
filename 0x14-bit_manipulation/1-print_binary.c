c8f24441dd8fd942841694a49767f3e2191b34c8
Wambu-i7 <wambuindikiru@gmail.com>
2023-10-26 02:26:48 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,31 +5,11 @@
 *Return: Nothing.
 */
 
-
 void print_binary(unsigned long int n)
 {
-	int start_print = 0;
-	int i;
-	unsigned long int bits;
-
-
-	for (i = sizeof(unsigned long int) * 8 - 1; i >= 0; i--)
+	if (n > 1)
 	{
-	bits = 1UL << i;
-	if (n & bits)
-	{
-	start_print = 1;
-	_putchar('1');
+	print_binary(n >> 1);
 	}
-	else if (start_print)
-	{
-	putchar('0');
-	}
-	}
-
-
-	if (!start_print)
-	{
-	_putchar('0');
-	}
+	_putchar((n & 1) ? '1' : '0');
 }
