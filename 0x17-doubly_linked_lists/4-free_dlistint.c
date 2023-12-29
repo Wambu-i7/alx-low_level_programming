01b300e8b2a386b93fcfc00a9406a68ea0d90d48
Wambu-i7 <wambuindikiru@gmail.com>
2023-12-29 03:56:23 -0800

    Free memory

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: Pointer to the head of the list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	dlistint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
