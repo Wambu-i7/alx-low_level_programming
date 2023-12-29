72ab93ff2d3f921ce09de1e67dce5bb0f2568036
Wambu-i7 <wambuindikiru@gmail.com>
2023-12-28 21:39:53 -0800

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (!newNode)
+	{
+	return NULL;
+	}
 	new_node->n = n;
 	new_node->next = (*head);
 	new_node->prev = NULL;
